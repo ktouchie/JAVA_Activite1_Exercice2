f57c6c751c61d26a7db001b08abf9bf219614d18
Karin Touchie <ktouchie@gmail.com>
2015-12-08 16:23:47 +0100

    Change prompts and variables for Exercice2

--- Exercice2.java
+++ Exercice2.java
@@ -3,12 +3,12 @@ class Exercice1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the base of the triangle:");
-        int base = sc.nextInt();
-        System.out.println("Enter the height of the triangle:");
-        int height = sc.nextInt();
-        int area = ((base*height)/2);
-        System.out.println("The area of the triangle is " + area);
+        System.out.println("Veuillez saisir le prix unitaire HT:");
+        int prix = sc.nextInt();
+        System.out.println("Veuillez saisir la quantité de produit:");
+        int quant = sc.nextInt();
+        int ttc = ((base*height)/2);
+        System.out.println("Le prix TTC est " + ttc);
 
 	}
 
