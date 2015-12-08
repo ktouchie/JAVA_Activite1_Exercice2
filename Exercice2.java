291f9884a030711dd8aa93da555ea9c9c1b06ad5
Karin Touchie <ktouchie@gmail.com>
2015-12-08 16:46:04 +0100

    Change int tva to float

--- Exercice2.java
+++ Exercice2.java
@@ -6,12 +6,12 @@ class Exercice2 {
         System.out.println("Veuillez saisir le prix unitaire HT:");
         int prix = sc.nextInt();
         System.out.println("Veuillez saisir le taux de TVA (%):");
-        int tva = sc.nextInt();
+        float tva = sc.nextInt();
         System.out.println("Veuillez saisir la quantite de produit:");
         int quant = sc.nextInt();
         int prix2 = (prix*quant);
         float ttc = (prix2+(prix2*(tva/100)));
-        System.out.println("Le prix TTC est " + ttc);
+        System.out.println("Le prix TTC est €" + ttc);
 
 	}
 
